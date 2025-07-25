38275aed933e97fdaa21a5b251e2f9711963e467
gglolppc <gglolppc@gmail.com>
2025-07-25 19:25:44 +0300

    model update

--- app/static/js/models.js
+++ app/static/js/models.js
@@ -1,78 +1,57 @@
 export const models = {
   apple: [
-    "iphone_16_Pro_Max",
-    "iphone_16_Pro",
-    "iphone_16_Plus",
-    "iphone_16",
-    "iphone_15_Pro_Max",
-    "iphone_15_Pro",
-    "iphone_15_Plus",
-    "iphone_15",
-    "iphone_14_Pro_Max",
-    "iphone_14_Pro",
-    "iphone_14_Plus",
-    "iphone_14",
-    "iphone_13_Pro_Max",
-    "iphone_13_Pro",
-    "iphone_13_Mini",
-    "iphone_13",
-    "iphone_12_Pro_Max",
-    "iphone_12_Pro",
-    "iphone_12_Mini",
-    "iphone_12",
-    "iphone_SE_3", // 2022
-    "iphone_11_Pro_Max",
-    "iphone_11_Pro",
-    "iphone_11",
-    "iphone_Xs_Max",
-    "iphone_Xs",
-    "iphone_Xr",
-    "iphone_X",
-    "iphone_8_Plus",
-    "iphone_8",
-    "iphone_7_Plus",
-    "iphone_7"
+    "iphone_16_Pro_Max", "iphone_16_Pro", "iphone_16_Plus", "iphone_16",
+    "iphone_15_Pro_Max", "iphone_15_Pro", "iphone_15_Plus", "iphone_15",
+    "iphone_14_Pro_Max", "iphone_14_Pro", "iphone_14_Plus", "iphone_14",
+    "iphone_13_Pro_Max", "iphone_13_Pro", "iphone_13_Mini", "iphone_13",
+    "iphone_12_Pro_Max", "iphone_12_Pro", "iphone_12_Mini", "iphone_12",
+    "iphone_SE_3", "iphone_SE_2", "iphone_11_Pro_Max", "iphone_11_Pro", "iphone_11",
+    "iphone_Xs_Max", "iphone_Xr", "iphone_X_XS",
+    "iphone_8_Plus", "iphone_8", "iphone_7_Plus", "iphone_7"
   ],
-  samsung: [
-      "s25_ultra",
-      "s25_plus",
-      "s25",
+  samsungs: [
+    "s25_ultra", "s25_plus", "s25",
+    "s24_ultra", "s24_plus", "s24", "s24_fe",
+    "s23_ultra", "s23_plus", "s23", "s23_fe",
+    "s22_ultra", "s22_plus", "s22",
+    "s21_ultra", "s21_plus",
+    "s20_ultra", "s20_plus", "s20", "s20_fe",
+    "s10_plus", "s10", "s10_lite",
+    "s9_plus", "s9",
+    "s8_plus", "s8"
+  ],
+  samsunga: [
+    "a73", "a72", "a71", "a70",
+    "a56", "a55", "a54", "a53", "a52", "a51", "a50",
+    "a36", "a35", "a34", "a33", "a32", "a31", "a30",
+    "a26", "a25", "a24", "a23", "a22", "a21S", "a20",
+    "a16", "a15", "a14", "a13", "a12", "a11",
+    "a06", "a05", "a05s", "a04", "04s", "a04e", "a02", "a02s", "a03", "a03s", "a03_core",
+  ],
+  samsungm: [
+    "m55", "m54", "m53", "m52", "m51",
+    "m35", "m34", "m33", "m32",
+    "m23", "m22",
+    "m13", "m12", "m11"
+  ],
+  redmi: [
+  // Redmi Note серия
+  "redmi_note_14_pro_4g", "redmi_note_14_4g", "redmi_note_14_pro_5g", "redmi_note_14_5g", "redmi_note_14s",
+  "redmi_note_13_pro_plus_5g", "redmi_note_13_pro_4g", "redmi_note_13_4g", "redmi_note_13_pro_5g", "redmi_note_13_5g",
+  "redmi_note_12S", "redmi_note_12_pro_4g", "redmi_note_12_4g", "redmi_note_12_pro_5g", "redmi_note_12_5g",
+  "redmi_note_11_pro", "redmi_note_11s", "redmi_note_11",
+  "redmi_note_10_pro", "redmi_note_10s", "redmi_note_10", "redmi_note_10_pro_5g",
+  "redmi_note_9_pro", "redmi_note_9s", "redmi_note_9",
+  "redmi_note_8",
 
-      "s24_ultra",
-      "s24_plus",
-      "s24",
-
-      "s23_ultra",
-      "s23_plus",
-      "s23",
-
-      "s22_ultra",
-      "s22_plus",
-      "s22",
-
-      "s21_ultra",
-      "s21_plus",
-      "s21",
-
-      "s20_ultra",
-      "s20_plus",
-      "s20",
-
-      "s10_5g",
-      "s10_plus",
-      "s10",
-      "s10e",
-
-      "s9_plus",
-      "s9",
-
-      "s8_plus",
-      "s8"
-    ],
-      xiaomi: [
-      "s25_ultra",
-      "s25_plus",
-      "s25"
-    ],
+  // Обычные Redmi
+  "redmi_14c", "redmi_14",
+  "redmi_13c", "redmi_13",
+  "redmi_12c", "redmi_12",
+  "redmi_11c", "redmi_11",
+  "redmi_10c", "redmi_10",
+  "redmi_9c", "redmi_9",
+  "redmi_8"
+]
 
 };
